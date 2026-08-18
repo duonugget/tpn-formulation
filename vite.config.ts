317547597b49e0f33b9d0d707afdad486f8461a1
duonugget <duonghuy335@gmail.com>
2026-08-19 06:30:04 +0700

    Deploy site to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/tpn-formulation/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
